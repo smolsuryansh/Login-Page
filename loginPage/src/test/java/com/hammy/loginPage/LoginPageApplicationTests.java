0ab9e1e7632fb26fb76789faf1c3492392b591d7
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-08-31 00:42:30 +0530

    Initial commit

---
+++ loginPage/src/test/java/com/hammy/loginPage/LoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hammy.loginPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
